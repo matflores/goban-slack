1f31447537751309ec5b39f9bed8008ba734eaa9
Matías Flores <flores.matias@gmail.com>
2015-05-22 11:22:41 -0300

    Create a new board with each request

--- lib/goban.rb
+++ lib/goban.rb
@@ -1,5 +1,11 @@
+require "net/http"
+
 module Goban
   def self.create
-    "http://goban.co/boards/p/269Cn0b"
+    uri  = URI("http://goban.co/boards")
+    res  = Net::HTTP.post_form(uri, "board[hash]" => "")
+    json = JSON.parse(res.body)
+
+    "http://goban.co/boards/p/#{json["hash"]}"
   end
 end
