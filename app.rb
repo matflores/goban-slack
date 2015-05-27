a703aca79b5f9188c2c46341bd526692e2e66427
Matías Flores <flores.matias@gmail.com>
2015-05-27 08:38:17 -0300

    Validate board size

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require_relative "lib/goban"
 require_relative "lib/slack"
 
 DEFAULT_BOARD_SIZE = 19
+VALID_BOARD_SIZES  = [9, 13, 19]
 
 Cuba.define do
   def forbidden!
@@ -18,15 +19,21 @@ Cuba.define do
     board_size = req.params["text"].to_i
     board_size = DEFAULT_BOARD_SIZE if board_size == 0
 
-    board_url = Goban.create(board_size)
+    if VALID_BOARD_SIZES.include?(board_size)
+      board_url = Goban.create(board_size)
 
-    payload = {
-      channel:    req.params["channel_id"],
-      username:   "Goban",
-      icon_emoji: ":black_circle:",
-      text:       "#{req.params["user_name"]} started a new game: <#{board_url}>"
-    }
+      payload = {
+        channel:    req.params["channel_id"],
+        username:   "Goban",
+        icon_emoji: ":black_circle:",
+        text:       "#{req.params["user_name"]} started a new game: <#{board_url}>"
+      }
 
-    Slack.post(payload)
+      Slack.post(payload)
+
+      res.status = 200
+    else
+      res.write("Invalid board size: #{board_size}. Valid board sizes are #{VALID_BOARD_SIZES.join(", ")}.")
+    end
   end
 end
